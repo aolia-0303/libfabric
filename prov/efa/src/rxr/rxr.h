6c0fc6ad082477d56bbe3cf33a194aa167734e99
Ao Li <aolia@amazon.com>
2020-07-15 18:24:13 +0000

    prov/efa: Long RTM and Long MSG FI_DELIVERY_COMPLETE
    This patch add FI_DELIVERY_COMPLETE support
    for long MSG & long RTW packets.
    
    Signed-off-by: Ao Li <aolia@amazon.com>

--- prov/efa/src/rxr/rxr.h
+++ prov/efa/src/rxr/rxr.h
@@ -350,6 +350,8 @@ struct rxr_rx_entry {
 	uint16_t credit_request;
 	int credit_cts;
 
+	bool delivery_complete_requested;
+
 	uint64_t total_len;
 
 	enum rxr_rx_comm_type state;
@@ -424,6 +426,8 @@ struct rxr_tx_entry {
 	int64_t window;
 	uint16_t credit_request;
 	uint16_t credit_allocated;
+
+	bool delivery_complete_requested;
 
 	uint64_t total_len;
 
